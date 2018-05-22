6750dc72e5a8e53040fc7641cb604a1c64bcc65c
MattJasonLu <1132464757@qq.com>
2018-05-22 18:07:54 +0800

    首页到产废平台的跳转已完成，哈哈哈哈哈哈哈

--- src/com/jdlink/controller/UserController.java
+++ src/com/jdlink/controller/UserController.java
@@ -5,13 +5,17 @@ import com.jdlink.domain.User;
 import com.jdlink.service.ClientService;
 import com.jdlink.service.UserService;
 import com.sun.javafx.sg.PGShape;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,43 +36,38 @@ public class UserController {
         ModelAndView mav = new ModelAndView();
         List<User> userList = userService.list();
         mav.addObject("userList", userList);
-        mav.setViewName("listUser");
+        mav.setViewName("/WEB-INF/jsp/listUser.jsp");
         return mav;
     }
 
     @RequestMapping("getUser")
-    public ModelAndView getUser(User user, HttpSession session) {
-        ModelAndView mav = new ModelAndView("wastesPlatform.html");
+    @ResponseBody
+    public User getUser(User user, HttpSession session) {
         try {
+            // 查询参数
             Map<String, String> params = new HashMap<>();
-
             if (user.getUsername().equals("") || user.getPassword().equals("")) {
-                throw new Exception("用户名或密码为空，请重试！");
+                return null;
             }
-
             params.put("username", user.getUsername());
             params.put("password", user.getPassword());
             List<User> userList = userService.get(params);
             // 更新用户，通过数据查询后得到的用户为准
             user = userList.get(0);
             if (userList.size() > 0) {
-                mav.addObject("username", user.getUsername());
-//                mav.setViewName("home");
                 // 将用户绑定的客户对象信息绑定到session中
                 Client client = clientService.getByClientId(user.getClientId());
                 // 将用户对应的客户编号或者管理员编号加入session保存
                 session.setAttribute("client", client);
                 // 设置session的有效时间为30分钟
                 session.setMaxInactiveInterval(30 * 60);
+                return user;
             } else {
-                throw new Exception("用户名密码错误，请重试！");
+                return null;
             }
         } catch (Exception e) {
-//            e.printStackTrace();
-            mav.addObject("message", "无法连接至服务器，请稍后重试！");
-            mav.setViewName("fail");
+            return null;
         }
-        return mav;
     }
 
     @RequestMapping("addUser")
